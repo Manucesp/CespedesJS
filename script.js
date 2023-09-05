a8e638dd6abbe1486494f92b7413a4c250063975
Manucesp <manucespedes24@gmail.com>
2023-09-05 11:31:14 -0300

    Modificacion en variables

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let intentos = 3;
+const intentos = 3;
 let contraseña;
 let validar;
 
 while(intentos > 0){
-     contraseña1 = prompt("Ingrese su contraseña ");
+     contraseña = prompt("Ingrese su contraseña ");
      validar = prompt("Valide su contraseña tienes " + intentos + " intentos");
     
-
-     if (contraseña.length < 8) {
+      if (contraseña.length < 8) {
         alert("La contraseña debe tener al menos 8 caracteres.");
     } else {
         validacion(contraseña, validar);
     }
+
     if(intentos == 0){
         alert("Cuenta bloqueada temportalmente , comunicate con el administrador");
     }
@@ -24,6 +24,6 @@ function validacion(){
     } else{
         alert("Tu contraseña no coincide con la que ingresaste");
     }
-
-     intentos--;
+    
+    intentos--;
 }
