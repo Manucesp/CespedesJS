80dad3a6a9594b908ed8ce2f8254bc88ad54f81a
Manucesp <manucespedes24@gmail.com>
2023-09-05 12:51:53 -0300

    Pre entrega finalizada

--- script.js
+++ script.js
@@ -2,28 +2,28 @@ let intentos = 3;
 let contraseña;
 let validar;
 
-while(intentos > 0){
-     contraseña = prompt("Ingrese su contraseña ");
-     validar = prompt("Valide su contraseña tienes " + intentos + " intentos");
-    
-      if (contraseña.length < 8) {
+while (intentos > 0) {
+    contraseña = prompt("Ingrese su contraseña ");
+
+    if (contraseña.length < 8) {
         alert("La contraseña debe tener al menos 8 caracteres.");
     } else {
+        validar = prompt("Valide su contraseña tienes " + intentos + " intentos");
         validacion(contraseña, validar);
     }
 
-    if(intentos == 0){
-        alert("Cuenta bloqueada temportalmente , comunicate con el administrador");
+    if (intentos == 0) {
+        alert("Cuenta bloqueada temporalmente, comuníquese con el administrador");
     }
 }
 
-function validacion(){
-    if(contraseña === validar){
+function validacion(contraseña, validar) {
+    if (contraseña === validar) {
         alert("Tu contraseña ha sido validada correctamente ");
         intentos = -1;
-    } else{
+    } else {
         alert("Tu contraseña no coincide con la que ingresaste");
     }
-    
+
     intentos--;
 }
